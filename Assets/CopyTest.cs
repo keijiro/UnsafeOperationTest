0c782b6c2c014fd2b5bd944ce0c57a0154ab22a7
Keijiro Takahashi <keijiro@gmail.com>
2018-01-22 10:25:22 +0900

    GCHandle example

--- Assets/CopyTest.cs
+++ Assets/CopyTest.cs
@@ -16,9 +16,16 @@ public class CopyTest : MonoBehaviour
     {
         var na = new NativeArray<byte>(_buffer.Length, Allocator.Temp);
 
+        // Use NativeArray.CopyTo (naive implementation; very slow)
+        //na.CopyTo(_buffer);
+
+        // Use Marshal to copy from unmanaged to managed (fast)
         Marshal.Copy(na.GetUnsafePtr(), _buffer, 0, _buffer.Length);
 
-        //na.CopyTo(_buffer);
+        // Use GCHandle to retrieve pointer to managed array (fast)
+        //var gch = GCHandle.Alloc(_buffer, GCHandleType.Pinned);
+        //UnsafeUtility.MemCpy(gch.AddrOfPinnedObject(), na.GetUnsafePtr(), (ulong)_buffer.Length);
+        //gch.Free();
 
         na.Dispose();
     }
